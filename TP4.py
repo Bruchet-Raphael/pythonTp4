85d1cf8c0ede9d78bde4d441bdae60c19b7a8d40
Raphael <raphael.bruchet80800@gmail.com>
2026-03-16 15:27:55 +0100

    Exo4

--- TP4.py
+++ TP4.py
@@ -1,11 +1,17 @@
 import pandas as pd
 import random as rd
 
-data = pd.read_csv("donnevrai.csv")
-data["danger"] = data["danger"].map({0: "safe", 1: "danger"})
+def Zpredict(x):
+    w = rd.randint(-10,10)
+    b = w*x
+    Z = w*x + b
+    if(Z >= 0):
+        return 1
+    else:
+        return 0
 
-w = rd.randint(-10,10)
-b = rd.randint(-10,10)
+data = pd.read_csv("donnevrai.csv")
+#data["danger"] = data["danger"].map({0: "safe", 1: "danger"})
 
 X = data["vitesse"].tolist()
 Y = data["danger"].tolist()
@@ -13,4 +19,14 @@ Y = data["danger"].tolist()
 for i in range(100):
     print("Vitesse :", X[i],":",Y[i])
 
-print(w,b)
+print(X[:5], Y[:5])
+
+for x in X:
+    print(f"x = {x},prédiction = {Zpredict(x)}")
+
+for x,y in zip(X,Y):
+    laPreY = Zpredict(x)
+    erreur = y - laPreY
+    if(erreur < 0):
+        erreur = 1
+    print(f"x = {x},y = {y},Y_pred = {laPreY}, Erreur = {erreur}")
